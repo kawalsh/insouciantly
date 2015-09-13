e031da82bcb3f0d220a4e99283a5d1211da170a2
walshka <kwalsh@spoonflower.com>
2015-09-13 11:38:32 -0400

    tweak to tags and add link to admin for patterns

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -14,7 +14,10 @@ class TagsController < ApplicationController
     if params[:view].nil? or params[:view] == ""
       if @tag.tag_type == "page"
         params[:view] = "blogs"
-      elsif @tag.tag_type == "company" or @tag.tag_type == "archive" or @tag.tag_type == "pattern"
+      elsif @tag.tag_type == "pattern"
+        pattern = Pattern.where(name: @tag.tag).take
+        redirect_to pattern_path(pattern)
+      elsif @tag.tag_type == "company" or @tag.tag_type == "archive"
         params[:view] = "companies"
       elsif @tag.tag_type == "genre"
         params[:view] = "books"
